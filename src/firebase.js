19719a9fd6f97a8292ff2b6aa98b800e7a422e92
Syed Faiz Ahmed <sfahmed129@gmail.com>
2025-06-14 00:04:37 +0500

    build

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBATNZgTQ4kQJXJMcSWNsHeYT9ggXiHufo",
-  authDomain: "e-commerce-f6c4d.firebaseapp.com",
-  projectId: "e-commerce-f6c4d",
-  storageBucket: "e-commerce-f6c4d.firebasestorage.app",
-  messagingSenderId: "159781970613",
-  appId: "1:159781970613:web:1ea7d45b52a9b1abc2d4fc",
-  measurementId: "G-D9ZV7YNYDR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-// const analytics = getAnalytics(app);
